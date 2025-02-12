e0ccb87c3656533a3e149b66c37868468f1ba791
Pablo Rafael <pabloofael@gmail.com>
2025-02-12 19:45:05 -0300

    feat: Adicionar o themeToggle e menu da conta

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import './global.css'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
       <RouterProvider router={router} />
       <Toaster />
-    </>
+    </ThemeProvider>
   )
 }
